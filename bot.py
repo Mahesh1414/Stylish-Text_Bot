29b626c351b29771a218d50a7ce3fcc8eac54cc3
Mahesh1414 <89467357+Mahesh1414@users.noreply.github.com>
2021-11-05 19:54:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,6 +17,7 @@ if __name__ == "__main__" :
         root="plugins"
     )
     app = Client(
+      "ShowJson",
         bot_token=Config.BOT_TOKEN,
         api_id=Config.API_ID,
         api_hash=Config.API_HASH,
